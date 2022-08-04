00976e35adebad72f6e57d7d0b1b0e654ebea363
hseka <hseka@lescollectionneurs.com>
2022-08-04 11:10:56 +0200

    Section 2 - 54 : Getters & else-if - Suite et clean code

--- flutter_complete_guide/lib/result.dart
+++ flutter_complete_guide/lib/result.dart
@@ -3,16 +3,20 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
   final int resultScore;
 
+  // Constructor (with positional argument) :
   Result(this.resultScore);
 
+  // Getter :
   String get resultPhrase {
-    var resultText = 'You did it !';
+    String resultText;
     if(resultScore <= 8) {
       resultText = 'You are awesome and innocent !';
     } else if (resultScore <= 12) {
       resultText = 'Pretty likeable !';
     } else if (resultScore <= 16) {
       resultText = 'You are... strange ?!';
+    } else {
+      resultText = 'You are so bad !';
     }
     return resultText;
   }
@@ -21,7 +25,10 @@ class Result extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Text(resultPhrase,
-        style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+        style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,),
     );
   }
 }
+
+
