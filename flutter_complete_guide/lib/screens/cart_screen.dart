c566477d3e5c012c7cc8bbdfb72da68613d90143
hseka <hseka@lescollectionneurs.com>
2022-08-23 15:13:35 +0200

    Section 8 - 206 : Displaying a List of Cart Items

--- flutter_complete_guide/lib/screens/cart_screen.dart
+++ flutter_complete_guide/lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/cart.dart';
+import '../providers/cart.dart'; // show Cart
+import '../widgets/cart_item.dart' as ci;
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -38,6 +39,19 @@ class CartScreen extends StatelessWidget {
                   )
                 ]
               )
+            )
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) =>
+                  ci.CartItem(
+                      cart.items.values.toList()[i].id,
+                      cart.items.values.toList()[i].price,
+                      cart.items.values.toList()[i].quantity,
+                      cart.items.values.toList()[i].title
+                  ),
             )
           )
         ]
