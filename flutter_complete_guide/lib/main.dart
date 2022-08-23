4f510bd276a33fa372d6cf7085be235a7af81d81
hseka <hseka@lescollectionneurs.com>
2022-08-23 16:36:39 +0200

    Section 8 - 209 : Providing an Orders Object

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/orders.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_complete_guide/widgets/product_item.dart';
 import './screens/products_overview_screen.dart';
@@ -6,6 +7,7 @@ import './screens/product_detail_screen.dart';
 import './screens/cart_screen.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
+import './providers/orders.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,6 +18,7 @@ class MyApp extends StatelessWidget {
         providers: [
           ChangeNotifierProvider(create: (ctx) => Products(),),
           ChangeNotifierProvider(create: (ctx) => Cart(),),
+          ChangeNotifierProvider(create: (ctx) => Orders(),),
       ],
         child: MaterialApp(
             title: 'MyShop',
