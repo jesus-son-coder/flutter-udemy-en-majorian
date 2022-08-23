69a9d06fc66a3b72768b0daa9509ef8414b2eab1
hseka <hseka@lescollectionneurs.com>
2022-08-23 11:33:44 +0200

    Section 8 - 203 : Working with Multiple Providers

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -4,26 +4,31 @@ import 'package:flutter_complete_guide/widgets/product_item.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
-      child: MaterialApp(
-          title: 'MyShop',
-          theme: ThemeData(
-              primarySwatch: Colors.purple,
-              accentColor: Colors.deepOrange,
-              fontFamily: 'Lato'
-          ),
-          home: ProductsOverviewScreen(),
-          routes: {
-            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-          }
-      )
+    // Nous avons 2 Providers dans la classe principale :
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (ctx) => Products(),),
+          ChangeNotifierProvider(create: (ctx) => Cart(),),
+      ],
+        child: MaterialApp(
+            title: 'MyShop',
+            theme: ThemeData(
+                primarySwatch: Colors.purple,
+                accentColor: Colors.deepOrange,
+                fontFamily: 'Lato'
+            ),
+            home: ProductsOverviewScreen(),
+            routes: {
+              ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+            }
+        )
     );
   }
 }
