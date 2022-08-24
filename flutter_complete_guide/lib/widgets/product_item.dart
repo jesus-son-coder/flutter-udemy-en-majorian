c5360ae70555a11d8cec453d9140382e361852ae
hseka <hseka@lescollectionneurs.com>
2022-08-24 14:35:46 +0200

    Section 9 - 217 : [Nouvelle méthode] Snackbars (Popup info) and Undoing "Add to Cart" Actions

--- flutter_complete_guide/lib/widgets/product_item.dart
+++ flutter_complete_guide/lib/widgets/product_item.dart
@@ -38,19 +38,17 @@ class ProductItem extends StatelessWidget {
                     icon: Icon(Icons.shopping_cart),
                     onPressed: () {
                       cart.addItem(product.id, product.price, product.title);
-                      Scaffold.of(context).hideCurrentSnackBar();
-                      Scaffold.of(context).showSnackBar(
-                          SnackBar(
-                              content: Text('Added item to cart', textAlign: TextAlign.center,),
-                              duration: Duration(seconds: 3),
-                            action: SnackBarAction(
-                              label: 'Annuler',
-                              onPressed: () {
-                                cart.removeSingleItem(product.id);
-                              },
-                            )
+                      ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text('Added item to cart', textAlign: TextAlign.center,),
+                          duration: Duration(seconds: 3),
+                          action: SnackBarAction(
+                            label: 'Annuler',
+                            onPressed: () {
+                              cart.removeSingleItem(product.id);
+                            },
                           )
-                      );
+                      ));
                     },
                     color: Theme.of(context).accentColor)),
           ),
