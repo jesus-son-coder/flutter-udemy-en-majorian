1e187ed2d922e41f8607b40a73eed11d7d64625a
hseka <hseka@lescollectionneurs.com>
2022-08-25 12:40:50 +0200

    Section 9 - 232 : Saving New Products

--- flutter_complete_guide/lib/providers/products.dart
+++ flutter_complete_guide/lib/providers/products.dart
@@ -49,8 +49,18 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  void addProduct() {
-    // _items.add(value);
+  void addProduct(Product product) {
+    final newProduct = Product(
+        id: DateTime.now().toString(),
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl
+    );
+    _items.add(newProduct);
+    // or add at the top of the list :
+    // _items.insert(0, newProduct);
+
     notifyListeners();
   }
 }
